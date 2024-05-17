e623aa32704fded6be27ea274b555b8f7c215665
ujin-slav <80926662+ujin-slav@users.noreply.github.com>
2024-05-17 12:48:53 +0300

    Update index.js

--- app/index.js
+++ app/index.js
@@ -1,22 +1,25 @@
-const client = require('prom-client');
 const express = require('express');
-
-const metricExporter = require('./metrics');
+const promBundle = require('express-prom-bundle');
 
 const app = express();
 
-// Initialize metrics
-const registry = new client.Registry();
-metricExporter(registry);
-
-// Report Prometheus metrics on /metrics
-app.get('/metrics', async (req, res, next) => {
-  res.set('Content-Type', registry.contentType);
-  res.end(registry.metrics());
-  
-  next();
+promBundle.normalizePath = (req, opts) => {
+    return req.route?.path ?? "No";
+};
+// Init metrics
+const metricsMiddleware = promBundle({
+    includeMethod: true,
+    includePath: true,
+    buckets: [0.001, 0.01, 0.1, 1, 2, 3, 5, 7, 10, 15, 20, 25, 30, 35, 40, 50, 70, 100, 200],
+    customLabels: { model: "No" },
+    transformLabels: (labels, req, res) => {
+        labels.model = req?.body?.model ?? req?.body?.imageModel ?? req?.body?.voice ?? "No";
+        return labels;
+    },
 });
-
-
-// Run the server
-app.listen(9200, '0.0.0.0', () => console.log('App started!'));
+app.use(metricsMiddleware);
+// Routes 
+app.get('/data', (req, res) => {
+  res.json({data: 'Some data...'});
+}); 
+app.listen(3000);
