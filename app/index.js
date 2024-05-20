e372c2dd284e0dddcd1273e993b53a291444f78f
ujin-slav <80926662+ujin-slav@users.noreply.github.com>
2024-05-20 13:40:58 +0300

    Update index.js

--- app/index.js
+++ app/index.js
@@ -1,28 +1,49 @@
 const express = require('express');
-const promBundle = require('express-prom-bundle');
-
+const promMid = require('express-prometheus-middleware');
 const app = express();
 
-promBundle.normalizePath = (req, opts) => {
-    return req.route?.path ?? "No";
-};
-// Init metrics
-const metricsMiddleware = promBundle({
-    includeMethod: true,
-    includePath: true,
-    buckets: [0.001, 0.01, 0.1, 1, 2, 3, 5, 7, 10, 15, 20, 25, 30, 35, 40, 50, 70, 100, 200],
-    customLabels: { model: "No" },
-    transformLabels: (labels, req, res) => {
-        labels.model = req?.body?.model ?? req?.body?.imageModel ?? req?.body?.voice ?? "No";
-        return labels;
-    },
+const PORT = 9200;
+app.use(promMid({
+  metricsPath: '/metrics',
+  collectDefaultMetrics: true,
+  requestDurationBuckets: [0.1, 0.5, 1, 1.5],
+  requestLengthBuckets: [512, 1024, 5120, 10240, 51200, 102400],
+  responseLengthBuckets: [512, 1024, 5120, 10240, 51200, 102400],
+  /**
+   * Uncomenting the `authenticate` callback will make the `metricsPath` route
+   * require authentication. This authentication callback can make a simple
+   * basic auth test, or even query a remote server to validate access.
+   * To access /metrics you could do:
+   * curl -X GET user:password@localhost:9091/metrics
+   */
+  // authenticate: req => req.headers.authorization === 'Basic dXNlcjpwYXNzd29yZA==',
+  /**
+   * Uncommenting the `extraMasks` config will use the list of regexes to
+   * reformat URL path names and replace the values found with a placeholder value
+  */
+  // extraMasks: [/..:..:..:..:..:../],
+  /**
+   * The prefix option will cause all metrics to have the given prefix.
+   * E.g.: `app_prefix_http_requests_total`
+   */
+  // prefix: 'app_prefix_',
+  /**
+   * Can add custom labels with customLabels and transformLabels options
+   */
+  // customLabels: ['contentType'],
+  // transformLabels(labels, req) {
+  //   // eslint-disable-next-line no-param-reassign
+  //   labels.contentType = req.headers['content-type'];
+  // },
+}));
+
+// curl -X GET localhost:9091/hello?name=Chuck%20Norris
+app.get('/hello', (req, res) => {
+  console.log('GET /hello');
+  const { name = 'Anon' } = req.query;
+  res.json({ message: `Hello, ${name}!` });
 });
-app.use(metricsMiddleware);
-// Routes 
-app.get('/data', (req, res) => {
-  res.json({data: 'Some data...'});
-}); 
-app.get('/data1', (req, res) => {
-  res.json({data: 'Some data1...'});
-}); 
-app.listen(9200);
+
+app.listen(PORT, () => {
+  console.log(`Example api is listening on http://localhost:${PORT}`);
+});
